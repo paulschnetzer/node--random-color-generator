ef727e88ffd51977ae4e1009aacf9354a9b5e46e
Paul Schnetzer <1998sheldon@gmail.com>
2020-09-11 15:40:34 +0200

    fixed

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const command = process.argv;
 const chalk = require('chalk');
 const randomColor = require('randomcolor');
-// importiet alle notwendigen libaries und den userInput bzw. command
 
-const y = randomColor({
+const command = process.argv;
+const UserInput = randomColor({
   hue: command[2],
   luminosity: command[3],
 });
-
-//
 
 const randomColors = randomColor();
 if (command[2] === undefined) {
@@ -21,9 +18,9 @@ if (command[2] === undefined) {
   );
 } else {
   console.log(
-    chalk.hex(y)(
+    chalk.hex(UserInput)(
       '############################ \n############################\n######     ' +
-        y +
+        UserInput +
         '    ######\n############################ \n############################',
     ),
   );
